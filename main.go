2e78dbd565256706514d675cf517d7ef9377ce3e
bohrasd <bohrasdf@gmail.com>
2021-06-13 14:32:23 +0800

    WIP

--- main.go
+++ main.go
@@ -1,9 +1,81 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"strings"
+)
+
+func process(conn net.Conn) {
+
+	// dict to search
+	dic := map[string]string{
+		"a": "asd",
+		"b": "def",
+		"c": "ghi",
+	}
+
+	b := make([]byte, 1024)
+	n, err := conn.Read(b)
+	if err != nil {
+		fmt.Printf("cannot read: %v", err)
+		return
+	}
+
+	word := string(b[:n])
+	fmt.Printf("%q\n", word)
+
+	body := strings.SplitN(word, " ", 2)
+	switch body[0] {
+	case "GET":
+		stripped_str := strings.TrimSpace(body[1])
+		ans, ok := dic[stripped_str]
+		if !ok {
+			conn.Write([]byte("ERROR undefined"))
+		} else {
+			conn.Write([]byte("ANSWER " + ans))
+		}
+
+	case "SET":
+		item := strings.SplitN(body[1], " ", 2)
+		dic[item[0]] = item[1]
+	case "CLEAR":
+		dic = make(map[string]string)
+
+	case "ALL":
+		for k, v := range dic {
+			conn.Write([]byte(fmt.Sprintf("key: %s val: %v\n", k, v)))
+		}
+	}
+
+	conn.Close()
+
+	return
+}
 
 func main() {
 
-	fmt.Println("hello")
+	listener, err := net.Listen("tcp4", "127.0.0.1:30000")
+	if err != nil {
+		fmt.Printf("cannot listen: %v", err)
+		return
+	}
+
+	for {
+
+		conn, err := listener.Accept()
+
+		if err != nil {
+			fmt.Printf("cannot accept: %v", err)
+			return
+		}
+
+		go process(conn)
+
+	}
+
+	// b := make([]byte, 1024)
+	// in := os.Stdin
+	// n, err := in.Read(b)
 
 }
---
+++ main_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net"
+	"testing"
+)
+
+func TestHttp(t *testing.T) {
+	go main()
+
+	tests := []struct {
+		req, resp string
+	}{
+		{"GET a", "abc"},
+		{"GET c", "ghi"},
+		{"ALL", "key: c val: ghi\nkey: a val: asd\nkey: b val: def"},
+	}
+
+	resp_b := make([]byte, 1024)
+	var resp string
+	for _, test := range tests {
+		conn, _ := net.Dial("tcp4", "127.0.0.1:30000")
+
+		conn.Write([]byte(test.req))
+
+		n, _ := conn.Read(resp_b)
+		resp = string(resp_b[:n])
+		if resp != test.resp {
+			t.Errorf("%v", resp)
+		}
+		conn.Close()
+	}
+}
